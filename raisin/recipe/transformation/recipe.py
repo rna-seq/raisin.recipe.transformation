a10a8b7ffe3485e71d0c032b22fa9ac64017a861
Maik R��der <maikroeder@gmail.com>
2012-02-03 14:18:13 +0000

    =copy over files

--- raisin/recipe/transformation/recipe.py
+++ raisin/recipe/transformation/recipe.py
@@ -1,4 +1,21 @@
+import os
+import shutil
+
+import accessions
+import annotations
+import files
+import genomes
+import profiles
 import experiments
+
+import csv
+
+SOURCE_PATH = "../extract/workspace/%s"
+
+def read_csv(file_name):
+    return csv.DictReader(open(file_name, 'r'), 
+                               delimiter='\t', 
+                               skipinitialspace=True)
 
 class Recipe(object):
 
@@ -8,8 +25,23 @@ class Recipe(object):
         self.options = options
 
     def install(self):
-        staging = self.options['staging']
-        experiments.main(staging)
+        workspace = self.options['workspace']
+        data = {}
+        for file in ['accessions.csv',
+                     'profiles.csv',
+                     'annotations.csv',
+                     'files.csv',
+                     'genomes.csv']:
+            target = os.path.join(workspace, file)
+            shutil.copyfile(SOURCE_PATH % file, target)
+            data[file] = read_csv(target)
+
+        accessions.main(data, workspace)
+        annotations.main(data, workspace)
+        files.main(data, workspace)
+        genomes.main(data, workspace)
+        profiles.main(data, workspace)
+        experiments.main(data, workspace)
         
     def update(self):
         return self.install()
