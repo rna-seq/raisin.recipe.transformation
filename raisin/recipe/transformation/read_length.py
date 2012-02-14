f821e750989c8eda65e598096ede4500577c1651
Maik R��der <maikroeder@gmail.com>
2012-02-14 23:05:45 +0000

    =refactor read_length

--- raisin/recipe/transformation/read_length.py
+++ raisin/recipe/transformation/read_length.py
@@ -1,82 +1,37 @@
 import os
 
+
 def read_length(accession):
-    if not 'readType' in accession:
-        read_length = ''
-    elif accession['readType'] == '2x76D':
-        read_length = 76
-    elif accession['readType'] == '1x70D':
-        read_length = 70
-    elif accession['readType'] == '2x75':
-        read_length = 75
-    elif accession['readType'] == '1x80':
-        read_length = 80
-    elif accession['readType'] == '1x40':
-        read_length = 40
-    elif accession['readType'] == '1x75D':
-        read_length = 75
-    elif accession['readType'] == '2x100':
-        read_length = 100
-    elif accession['readType'] == '2x96':
-        read_length = 96
-    elif accession['readType'] == '2x53':
-        read_length = 53
-    elif accession['readType'] == '2x76':
-        read_length = 76
-    elif accession['readType'] == '2x46':
-        read_length = 46
-    elif accession['readType'] == '2x35':
-        read_length = 35
-    elif accession['readType'] == '2x34':
-        read_length = 34
-    elif accession['readType'] == '100':
-        read_length = 100
-    elif accession['readType'] == '2x40':
-        read_length = 40
-    elif accession['readType'] == '2x50':
-        read_length = 50
-    elif accession['readType'] == '2x51':
-        read_length = 51
-    elif accession['readType'] == '2x54':
-        read_length = 54
-    elif accession['readType'] == '2x49':
-        read_length = 49
-    elif accession['readType'] in ['2x36', '1x36']:
-        read_length = 36
-    elif accession['readType'] == '2x37':
-        read_length = 37
-    elif accession['readType'] == '36':
-        read_length = 36
-    elif accession['readType'] == '50':
-        read_length = 50
-    elif accession['readType'] == '1x50':
-        read_length = 50
-    elif accession['readType'] == '75':
-        read_length = 75
-    elif accession['readType'] == '76':
-        read_length = 76
-    elif accession['readType'] == '1x76':
-        read_length = 76
-    elif accession['readType'] == '1x70D':
-        read_length = 70
-    elif accession['readType'] == '1x80D':
-        read_length = 80
+    """
+    Given a readType, parse the read length
+
+    readType can be for example:
+
+    2x50, 75D, 2x76D, 1x70D, 2x75, 1x80, 1x40, 1x75D, 2x100
+    2x96, 2x53, 2x76, 2x46, 2x35, 2x34, 100, 2x40, 2x50, 2x51
+    2x54, 2x49, 2x36, 1x36, 2x37, 50, 75
+    """
+    read_length = accession['readType']
+    if 'D' in read_length:
+        read_length = read_length.split('D')[0]
+    if  'x' in read_length:
+        # Extract the read length taking the value after the x
+        read_length = read_length.split('x')[1]
+    if read_length.isdigit():
+        return read_length
     else:
         print accession
         raise AttributeError
     return read_length
 
+
 def main(data, workspace):
-
-    views = {}
-
     path = os.path.join(workspace, "read_length.csv")
     file = open(path, "w")
     file.write("project_id\taccession_id\tread_length\n")
-    
+
     for accession in data['accessions.csv']:
         file.write("%s\t%s\t%s\n" % (accession['project_id'],
                                      accession['accession_id'],
                                      read_length(accession)))
     file.close()
-
