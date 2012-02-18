e36b79c04eeca7d08930d758a4535d1a5d9a1688
Maik R��der <maikroeder@gmail.com>
2012-02-18 20:20:49 +0000

    =report missing user

--- raisin/recipe/transformation/profiles.py
+++ raisin/recipe/transformation/profiles.py
@@ -1,2 +1,13 @@
-def main(data, workspace):
-    pass
+def main(buildout, data, workspace):
+    missing = []
+    for profile in data['profiles.csv']:
+        if not profile['PROJECTID'] in buildout['project_users']:
+            missing.append(profile['PROJECTID'])
+    if missing:
+        message = []
+        message.append("Add a user for the project in the project_users section:")
+        message.append("[project_users]")
+        for project in missing:
+            message.append("%s = anonymous" % project)
+        raise AttributeError("Missing configuration\n%s" % "\n".join(message))
+    
