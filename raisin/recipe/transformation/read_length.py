f6565a40e3ec4e9fb1c9e46691710309c40ab140
Maik R��der <maikroeder@gmail.com>
2012-03-20 10:31:59 +0000

    =read_length

--- raisin/recipe/transformation/read_length.py
+++ raisin/recipe/transformation/read_length.py
@@ -25,13 +25,66 @@ def read_length(accession):
     return read_length
 
 
+def get_accessions(data):
+    accessions = {}
+    for accession in data['accessions.csv']:
+        key = (accession['project_id'],
+               accession['accession_id'])
+        accessions[key] = accession
+    return accessions
+
+def get_runs(data):
+    runs = {}
+    for run in data['runs.csv']:
+        key = (run['project_id'],
+               run['run_id'])
+        runs[key] = run
+    return runs
+
+
 def main(data, workspace):
     path = os.path.join(workspace, "read_length.csv")
     file = open(path, "w")
-    file.write("project_id\taccession_id\tread_length\n")
+    headers = ["project_id", 
+               "accession_id", 
+               "run_id",
+               "read_length"]
+    file.write("\t".join(headers))
+    file.write("\n")
 
-    for accession in data['accessions.csv']:
-        file.write("%s\t%s\t%s\n" % (accession['project_id'],
-                                     accession['accession_id'],
-                                     read_length(accession)))
+    accessions = get_accessions(data)
+    runs = get_runs(data)
+
+    # Update run_id when going through the accessions
+    for key, accession in accessions.items():
+        project_id, accession_id = key
+        if key in runs:
+            accession['run_id'] = accession_id
+    
+    # Update accession_id when going through the runs
+    for key, run in runs.items():
+        project_id, run_id = key
+        if key in accessions:
+            run['accession_id'] = run_id
+
+    results = set()
+    for accession in accessions.values():
+        results.add((accession['project_id'],
+                     accession['accession_id'],
+                     accession.get('run_id', ''),
+                     read_length(accession)))
+    for run in runs.values():
+        results.add((run['project_id'],
+                     run.get('accession_id', ''),
+                     run['run_id'],
+                     run['read_length']))
+
+    results = list(results)
+    results.sort()
+    
+    for result in results:
+        if result[1] and result[2] and result[1] != result[2]:
+            print result
+        file.write("%s\t%s\t%s\t%s\n" % result)
+
     file.close()
