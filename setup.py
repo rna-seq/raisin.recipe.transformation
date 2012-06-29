37096d23a65bc695e4700a07c7d67b7f4437b791
Maik R��der <maikroeder@gmail.com>
2012-06-29 16:07:23 +0000

    =better meta data

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ from setuptools import setup, find_packages
 
 version = '1.1'
 
-long_description = ''
+long_description = """The raisin.recipe.transformation package is a Buildout recipe used
+for transforming data for the data warehouse of Raisin, the web application used for 
+publishing the summary statistics of Grape, a pipeline used for processing and analyzing
+RNA-Seq data. In ETL terms, it transforms the extracted data before loading it into the
+data warehouse."""
 entry_point = 'raisin.recipe.transformation:Recipe'
 entry_points = {"zc.buildout": [
                   "default = raisin.recipe.transformation:Recipe",
@@ -15,28 +19,33 @@ entry_points = {"zc.buildout": [
 
 setup(name='raisin.recipe.transformation',
       version=version,
-      description="raisin.recipe.transformation",
+      description="A Buildout recipe for transforming data for the Raisin data warehouse",
       long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
           'Framework :: Buildout',
           'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
+          'Environment :: Console',
           'Programming Language :: Python',
           'Intended Audience :: Developers',
           'Operating System :: OS Independent',
           'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
           'Topic :: Software Development :: Build Tools',
-          'Topic :: Software Development :: Libraries :: Python Modules',],
-      keywords='raisin recipe transformation',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Operating System :: POSIX :: Linux',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: System :: Installation/Setup'],
+      keywords='RNA-Seq pipeline ngs transcriptome bioinformatics ETL',
       author='Maik Roder',
-      author_email='roeder@berg.net',
-      url='http://big.crg.cat/bioinformatics_and_genomics',
+      author_email='maikroeder@gmail.com',
+      url='http://big.crg.cat/services/grape',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raisin', 'raisin.recipe'],
       include_package_data=True,
       zip_safe=False,
+      test_suite='grape.recipe.pipeline.tests',
       install_requires=['setuptools',
                         'zc.buildout',
                         # -*- Extra requirements: -*-
